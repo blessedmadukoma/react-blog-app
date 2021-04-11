272974da0707a2878772669b5a25ae723e9f93ca
Madukoma Blessed <blessedmadukoma@gmail.com>
2021-04-11 19:15:56 +0100

    lesson 7: Click Events and Functions

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Hello Ninjas', e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('Hello ' + name, e.target);
+  }
+
   return ( 
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e)=>handleClickAgain('mario', e)}>Click Me Again</button>
     </div>
    );
 }
