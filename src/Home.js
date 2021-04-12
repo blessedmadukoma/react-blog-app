2ba1827a7c1c6728d3a16a1e582766ac482b5f8d
Madukoma Blessed <blessedmadukoma@gmail.com>
2021-04-12 12:59:37 +0100

    lesson 15: useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -16,12 +18,15 @@ const Home = () => {
   // Useful for fetching
   useEffect(() => {
     console.log('use effect ran');
-  });
+    console.log(name);
+  }, [name]); // name is a dependency
 
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!" /> */}
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
    );
 }
