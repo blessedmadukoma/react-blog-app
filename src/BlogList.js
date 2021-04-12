30af1bfc95adb9dc3405a5d0ba648f228ad8ddb0
Madukoma Blessed <blessedmadukoma@gmail.com>
2021-04-12 12:22:47 +0100

    lesson 3: functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,8 @@
   // const blogs = props.blogs;
   // const title = props.title;
   // or
-const BlogList = ({ blogs, title}) => {
+const BlogList = ({ blogs, title, handleDelete}) => {
+
 
   return ( 
     <div className="blog-list">
@@ -11,6 +12,7 @@ const BlogList = ({ blogs, title}) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
